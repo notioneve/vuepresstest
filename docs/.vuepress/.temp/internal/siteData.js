489f1c51272c59655e220230457c17be9ed49853
Noc <1927079914@qq.com>
2021-12-17 20:08:54 +0800

    注释

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,16 @@
+export const siteData = {
+  "base": "/vuepresstest/",
+  "lang": "en-US",
+  "title": "notion的博客",
+  "description": "",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/avatar.png"
+      }
+    ]
+  ],
+  "locales": {}
+}
---
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -0,0 +1,45 @@
+export const themeData = {
+  "logo": "/avatar.png",
+  "nav": [
+    {
+      "text": "首页",
+      "link": "/"
+    },
+    {
+      "text": "技术文档",
+      "link": "/tech/interview/"
+    },
+    {
+      "text": "简书主页",
+      "link": "https://www.jianshu.com/u/c455567c7f50"
+    }
+  ],
+  "sidebar": "auto",
+  "locales": {
+    "/": {
+      "selectLanguageName": "English"
+    }
+  },
+  "navbar": [],
+  "darkMode": true,
+  "repo": null,
+  "selectLanguageText": "Languages",
+  "selectLanguageAriaLabel": "Select language",
+  "sidebarDepth": 2,
+  "editLink": true,
+  "editLinkText": "Edit this page",
+  "lastUpdated": true,
+  "lastUpdatedText": "Last Updated",
+  "contributors": true,
+  "contributorsText": "Contributors",
+  "notFound": [
+    "There's nothing here.",
+    "How did we get here?",
+    "That's a Four-Oh-Four.",
+    "Looks like we've got some broken links."
+  ],
+  "backToHome": "Take me home",
+  "openInNewWindow": "open in new window",
+  "toggleDarkMode": "toggle dark mode",
+  "toggleSidebar": "toggle sidebar"
+}
